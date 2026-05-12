9bc7d9e13d76f1259746ce2ba343c3e68bfe78da
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-05-12 23:36:03 +0900

    chore: implement base file readder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> hound::Result<()> {
+    let reader = hound::WavReader::open("./misc/voice.wav")?;
+    let spec = reader.spec();
+    println!("rate = {}, channels = {}", spec.sample_rate, spec.channels);
+    Ok(())
 }
