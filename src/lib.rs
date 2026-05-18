f19d2a7ce902e6046d1e5d793fb4245c78e5957b
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-05-18 11:25:12 +0900

    fix: pitch shift finished

--- src/lib.rs
+++ src/lib.rs
@@ -5,92 +5,64 @@ use realfft::RealFftPlanner;
 // DOCUMENT: https://en.wikipedia.org/wiki/Piano_key_frequencies
 static STEP: LazyLock<f32> = LazyLock::new(|| 2_f32.powf(1. / 12.));
 
-pub fn tone_up(data: &mut [f32], rate: u32, window: usize, tone: usize) -> Vec<f32> {
+pub fn tone_up(data: &mut [f32], window: usize, tone: usize) -> Vec<f32> {
     let mut planner = RealFftPlanner::<f32>::new();
     let mut output = vec![];
     for data_clip in data.chunks_mut(window) {
-        let freq = (rate as f32 / 2.) / data_clip.len() as f32;
-        let data_up = tone_up_inner(&mut planner, data_clip, freq, tone);
+        let data_up = tone_up_inner(&mut planner, data_clip, tone);
         output.extend(data_up);
     }
     output
 }
 
-fn tone_up_inner(
-    planner: &mut RealFftPlanner<f32>,
-    data: &mut [f32],
-    freq_step: f32,
-    tone: usize,
-) -> Vec<f32> {
+fn tone_up_inner(planner: &mut RealFftPlanner<f32>, data: &mut [f32], tone: usize) -> Vec<f32> {
     let data_len = data.len();
     let fft = planner.plan_fft_forward(data_len);
     let mut spectrum = fft.make_output_vec();
     fft.process(data, &mut spectrum).unwrap();
-    let mut max_freq = 0.;
-    let mut am_max = 0.;
-    for (i, complex) in spectrum.iter().enumerate() {
-        let am = complex.norm();
-        if am > am_max {
-            am_max = am;
-            max_freq = (i as f32) * freq_step;
+    let mut spectrum_up = vec![realfft::num_complex::Complex { re: 0., im: 0. }; spectrum.len()];
+    for (index, complex) in spectrum.iter().enumerate() {
+        let shift_index = (index as f32 * (*STEP).powi(tone as i32)) as usize;
+        if shift_index >= spectrum.len() {
+            spectrum_up[index] += 0.1 * complex;
+            continue;
         }
-    }
-    let mut shift = ((max_freq / *STEP) / freq_step) as u32 * tone as u32;
-    while shift > 0 {
-        spectrum.insert(0, realfft::num_complex::Complex { re: 0., im: 0. });
-        spectrum.pop();
-        shift -= 1;
+        spectrum_up[shift_index] += complex;
     }
 
-    spectrum.last_mut().unwrap().im = 0.;
+    spectrum_up.last_mut().unwrap().im = 0.;
     let ifft = planner.plan_fft_inverse(data.len());
     let mut output = ifft.make_output_vec();
-    ifft.process(&mut spectrum, &mut output).unwrap();
+    ifft.process(&mut spectrum_up, &mut output).unwrap();
     output.iter_mut().for_each(|o| *o /= data_len as f32);
 
     output
 }
-pub fn tone_down(data: &mut [f32], rate: u32, window: usize, tone: usize) -> Vec<f32> {
+pub fn tone_down(data: &mut [f32], window: usize, tone: usize) -> Vec<f32> {
     let mut planner = RealFftPlanner::<f32>::new();
     let mut output = vec![];
     for data_clip in data.chunks_mut(window) {
-        let freq = (rate as f32 / 2.) / data_clip.len() as f32;
-        let data_up = tone_down_inner(&mut planner, data_clip, freq, tone);
+        let data_up = tone_down_inner(&mut planner, data_clip, tone);
         output.extend(data_up);
     }
     output
 }
 
-fn tone_down_inner(
-    planner: &mut RealFftPlanner<f32>,
-    data: &mut [f32],
-    freq_step: f32,
-    tone: usize,
-) -> Vec<f32> {
+fn tone_down_inner(planner: &mut RealFftPlanner<f32>, data: &mut [f32], tone: usize) -> Vec<f32> {
     let data_len = data.len();
     let fft = planner.plan_fft_forward(data_len);
     let mut spectrum = fft.make_output_vec();
     fft.process(data, &mut spectrum).unwrap();
-    let mut max_freq = 0.;
-    let mut am_max = 0.;
-    for (i, complex) in spectrum.iter().enumerate() {
-        let am = complex.norm();
-        if am > am_max {
-            am_max = am;
-            max_freq = (i as f32) * freq_step;
-        }
-    }
-    let mut shift = ((max_freq / *STEP) / freq_step) as u32 * tone as u32;
-    while shift > 0 {
-        spectrum.push(realfft::num_complex::Complex { re: 0., im: 0. });
-        spectrum.remove(0);
-        shift -= 1;
+    let mut spectrum_down = vec![realfft::num_complex::Complex { re: 0., im: 0. }; spectrum.len()];
+    for (index, complex) in spectrum.iter().enumerate() {
+        let shift_index = (index as f32 / (*STEP).powi(tone as i32)) as usize;
+        spectrum_down[shift_index] += complex;
     }
 
-    spectrum[0].im = 0.;
+    spectrum_down[0].im = 0.;
     let ifft = planner.plan_fft_inverse(data.len());
     let mut output = ifft.make_output_vec();
-    ifft.process(&mut spectrum, &mut output).unwrap();
+    ifft.process(&mut spectrum_down, &mut output).unwrap();
     output.iter_mut().for_each(|o| *o /= data_len as f32);
 
     output
--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,15 @@ fn main() -> hound::Result<()> {
         spec.sample_rate, spec.channels, spec.sample_format
     );
 
-    let mut samples: Vec<f32> = reader
-        .samples::<i16>()
-        .map(|s| s.unwrap() as f32 / (i16::MAX as f32))
-        .collect();
+    let mut samples: Vec<f32> = reader.samples::<i16>().map(|s| s.unwrap() as f32).collect();
 
-    let tone_up = tone_down(&mut samples, spec.sample_rate, 560, 1);
+    let tone_up = tone_down(&mut samples, 1024, 10);
 
-    let mut writer = hound::WavWriter::create(
-        "./output.wav",
-        hound::WavSpec {
-            sample_format: hound::SampleFormat::Float,
-            bits_per_sample: 32,
-            channels: 2,
-            ..spec
-        },
-    )?;
+    let mut writer = hound::WavWriter::create("./output.wav", spec)?;
     for data in tone_up {
-        writer.write_sample(data)?;
-        writer.write_sample(data)?;
+        writer.write_sample(data as i16)?;
     }
     writer.finalize()?;
-    // What the fuck why they are different so much!
 
     Ok(())
 }
